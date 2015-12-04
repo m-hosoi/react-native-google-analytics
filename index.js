e7e0b9a3cc641ad4e00f9842a926dc77197d22e3
hosoi <hosoi@appland.co.jp>
2015-12-04 10:57:56 +0900

    fix: Hits.Item

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ module.exports = {
     PageView: require('./lib/hits/PageView'),
     ScreenView: require('./lib/hits/ScreenView'),
     Social: require('./lib/hits/Social'),
+    Timing: require('./lib/hits/Timing'),
     Transaction: require('./lib/hits/Transaction'),
-    Timing: require('./lib/hits/Timing')
+    Item: require('./lib/hits/Item'),
   },
   Analytics: require('./lib/Analytics'),
   Experiment: require('./lib/Experiment'),
